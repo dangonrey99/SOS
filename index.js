4d5e788a11e4ca416db5569a98de7ea8664e328d
Daniel Gonzalez <danieldecadiz@gmail.com>
2020-02-27 09:25:15 +0000

    primer lab

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const cool = require("cool-ascii-faces");
 
-var cool = require("cool-ascii-faces");
-console.log(cool());
+var app = express();
+
+app.get("/", (res, res)=>{
+	res.send("<html><body> Hello World </body> </html>");
+});
+
+app.listen(80);
+
+
+
+console.log("server ready");
